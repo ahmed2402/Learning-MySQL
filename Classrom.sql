93a1373af51b27195308be535f6190dd4e1ea3d8
ahmed2402 <ahmedraza312682@gmail.com>
2025-03-04 20:19:16 +0500

    done

--- Classrom.sql
+++ Classrom.sql
@@ -15,3 +15,24 @@ INSERT INTO student (id, name, age) VALUES
 INSERT INTO student VALUES (3,"YE",44);
 
 SELECT * FROM student;
+
+-- PRACTICE QUESTION
+CREATE DATABASE XYZ ;
+USE XYZ ;
+CREATE TABLE employee(
+	ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary FLOAT 
+);
+
+INSERT INTO employee (ID,Name,Salary) VALUES
+(1,"adam",25000),
+(2,"bob",30000),
+(3,"casey",40000);
+
+SELECT * FROM employee ;
+
+
+
+
+
