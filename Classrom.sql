5212bbfc75b43b4706e5afe59f88a42c16d541b7
ahmed2402 <ahmedraza312682@gmail.com>
2025-03-04 20:04:42 +0500

    notes added

--- Classrom.sql
+++ Classrom.sql
@@ -8,7 +8,10 @@ CREATE TABLE student(
     age INT NOT NULL
 );
 
-INSERT INTO student VALUES(1,"AHMED",20);
-INSERT INTO student VALUES(2,"YE",29);
+INSERT INTO student (id, name, age) VALUES
+(1,"ahmvd",20),
+(2,"carti",29);
+
+INSERT INTO student VALUES (3,"YE",44);
 
 SELECT * FROM student;
