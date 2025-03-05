12ed0b36ebfa42b93b869a6addf599f2c93ca491
ahmed2402 <ahmedraza312682@gmail.com>
2025-03-05 13:08:30 +0500

    SELECT AND WHERE CLAUSE

---
+++ phase2.sql
@@ -0,0 +1,47 @@
+-- CREATE DATABASE office;
+-- USE OFFICE;
+
+-- CREATE TABLE employee(
+-- 	id INT ,
+--     name VARCHAR(100),
+--     salary INT default 50000
+-- );
+
+-- INSERT INTO employee (id,name) VALUES
+-- (1,"ahmed"),
+-- (2,"anything");
+
+-- SELECT * FROM employee
+
+
+CREATE DATABASE college2;
+USE college2;
+
+CREATE TABLE student (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student
+(rollno, name, marks, grade, city)
+VALUES
+(101, "anil", 78, "C", "Pune"),
+(102, "bhumika", 93, "A", "Mumbai"),
+(103, "chetan", 85, "B", "Mumbai"),
+(104, "dhruv", 96, "A", "Delhi"),
+(105, "emanuel", 12, "F", "Delhi"),
+(106, "farah", 82, "B", "Delhi");
+
+SELECT * FROM student ;
+SELECT name,marks FROM student;
+SELECT city FROM student;
+SELECT DISTINCT city FROM student;
+SELECT * FROM student where marks > 80;
+SELECT * FROM student where city = "Delhi";
+
+SELECT *
+FROM student 
+WHERE marks > 80 AND city ="Delhi";
